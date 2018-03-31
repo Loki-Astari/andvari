3c75a210814787f3970a41b153865a99ad833c5f
Loki Astari <Loki.Astari@gmail.com>
2018-03-31 11:26:38 -0700

    Allow for alternative comment systems.
        We default to disque for now.

--- script/comment.js
+++ script/comment.js
@@ -1,4 +1,8 @@
 
 module.exports = function(post) {
-    return '| <a href="' + post.path.replace(/index.html/, '') + '#disqus_thread" data-disqus-identifier="' + post.permlink + '">Comments</a>';
+    var commentSystems = {
+        disque: (post) => 'data-disqus-identifier="' + post.permlink + '"',
+    };
+    var mark = commentSystems[post.comment](post);
+    return '| <a href="' + post.path.replace(/index.html/, '') + '#CommentSection" ' + mark + '>Comments</a>';
 }
