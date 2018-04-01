6513c4f68fd4ea0483ecf70361674b0d4505f31d
Loki Astari <Loki.Astari@gmail.com>
2018-04-01 11:58:56 -0700

    Allow a non default value be used to specify the time

--- script/time.js
+++ script/time.js
@@ -1,9 +1,9 @@
 
-module.exports = function(dateTime) {
-    // time datetime="Sun May 29 2016 21:13:39 GMT-0700 (PDT)" pubdate data-updated="true">May 29<span>th</span>, 2016</time>
-    // 2015-01-15 08:13:47 -0800
-    // <time datetime="<%- post.date %>" pubdate data-updated="true">May 29<span>th</span>, 2016</time>
+module.exports = function(dateTime, format) {
+    if (!format) {
+        format = 'mmm d "<span>"S"</span>", yyyy';
+    }
     var dt = new Date(dateTime);
     var dateFormat = require('dateformat');
-    return '<time datetime="' + dt.toString() + '" data-updated="true">' + dateFormat(dt, 'mmm d "<span>"S"</span>", yyyy') + '</time>';
+    return '<time datetime="' + dateFormat(dt, 'isoUtcDateTime') + '" pubdate>' + dateFormat(dt, format) + '</time>';
 }
