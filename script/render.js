c080a4ad7d04fce1e93498aa9a9ebbc628d75ba2
Loki Astari <Loki.Astari@gmail.com>
2018-04-02 19:00:12 -0700

    Need to expand variables in the mark down.

--- script/render.js
+++ script/render.js
@@ -1,10 +1,12 @@
 
 module.exports = function(post, size) {
+    var ejs = require('ejs');
     var showdown  = require('showdown');
     var converter = new showdown.Converter();
 
     var render = (size != -1)
                     ? post.content.substr(0, size) + '[...more](' + post.path +')'
                     : post.content;
+    render = ejs.render(render, this);
     return converter.makeHtml(render);
 }
